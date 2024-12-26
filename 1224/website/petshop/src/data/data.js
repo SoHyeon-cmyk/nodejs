73552ffea199cd025b3da1a032d275f01e726389
SoHyeon <thguskim12@gmail.com>
2024-12-26 15:06:27 +0900

    1226

---
+++ 1224/website/petshop/src/data/data.js
@@ -0,0 +1,25 @@
+let data =[
+  {
+    id:1
+    ,img:'/img/mainimage01.png'
+    ,textT:'슬기로운 펫캉스 생활'
+    ,textblod:"반려동물 동반여행 인기 여행지"
+    ,textLink:'javascript:;'
+  },
+  {
+    id:2
+    ,img:'/img/mainimage02.png'
+    ,textT:'2024 트랜드 어워즈'
+    ,textblod:"2024 트렌드 어워즈 대구석 연말 총결산"
+    ,textLink:'javascript:;'
+  },
+  {
+    id:3
+    ,img:'/img/mainimage03.png'
+    ,textT:'크리스마스에 진심이라면 주목'
+    ,textblod:"백화점에서 맞이하는 미리 크리스마스"
+    ,textLink:'javascript:;'
+  }
+  
+]
+export default data
--- 1224/website/petshop/src/pages/About.jsx
+++ 1224/website/petshop/src/pages/About.jsx
@@ -1,9 +1,4 @@
 import React from 'react';
-import {BrowserRouter} from 'react-router-dom'
-import App from '../App';
-
-
-// const root = ReactDOM.creat 
 
 const About = () => {
   return (
